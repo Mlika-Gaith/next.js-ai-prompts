660c8aa8750b2d3869a64103802f8c8cd68539db
ghaith_mlika <mlika.gaith@gmail.com>
2023-10-20 00:20:17 +0100

    implement react-form-hook and yup validations for email provider

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -1,13 +1,27 @@
 "use client";
 import { useEffect, useState } from "react";
-import { signIn, getProviders, useSession } from "next-auth/react";
+import { signIn, getProviders } from "next-auth/react";
 import Image from "next/image";
 import Spinner from "@components/Spinner";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const SignIn = () => {
-  const [email, setEmail] = useState("");
+  const validationSchema = yup.object().shape({
+    email: yup
+      .string()
+      .email("Invalid email format.")
+      .required("Email is required."),
+  });
+  const {
+    control,
+    handleSubmit,
+    formState: { errors, isDirty, isTouched },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
   const [providers, setProviders] = useState(null);
-  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     const setUpProviders = async () => {
@@ -17,30 +31,16 @@ const SignIn = () => {
     setUpProviders();
   }, []);
 
-  const validateEmail = (email) => {
-    // Regular expression for basic email validation
-    const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
-    return emailPattern.test(email);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    // Client-side email validation
-    if (!validateEmail(email)) {
-      setErrorMessage("Invalid email address");
-      return;
-    }
-
-    // Perform sign-in using the email provider
-    const result = await signIn("email", {
+  const onSubmit = async (data) => {
+    console.log(data);
+    /*const result = await signIn("email", {
       email,
-    });
+    });*/
 
-    if (result.error) {
+    /*if (result.error) {
       // Handle sign-in error (e.g., display error message).
       setErrorMessage(result.error);
-    }
+    }*/
   };
   if (providers != null) {
     return (
@@ -50,22 +50,30 @@ const SignIn = () => {
           Object.values(providers).map((provider) => {
             return provider.id === "email" ? (
               <>
-                <form onSubmit={handleSubmit} className="w-[90%] mt-4">
-                  <label className="form_label">
-                    Email
-                    <input
-                      type="email"
-                      value={email}
-                      placeholder="janedoe@gmail.com"
-                      className="form_input"
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </label>
-                  {errorMessage && (
-                    <div className="text-center text-red-500 text-sm">
-                      {errorMessage}
-                    </div>
-                  )}
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="w-[90%] mt-4"
+                >
+                  <label className="form_label">Email</label>
+                  <Controller
+                    name="email"
+                    control={control}
+                    render={({ field }) => (
+                      <>
+                        <input
+                          className="form_input"
+                          placeholder="janedoe@gmail.com"
+                          {...field}
+                        />
+                        {errors.email && isDirty && (
+                          <div className="text-center text-red-500 text-sm">
+                            {errors.email.message}
+                          </div>
+                        )}
+                      </>
+                    )}
+                  />
+
                   <button type="submit" className="form_input form_btn">
                     Sign In With Email
                   </button>
