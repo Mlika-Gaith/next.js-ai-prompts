aadb4454c0fb8449624dc040054284028448ffb3
ghaith_mlika <mlika.gaith@gmail.com>
2023-10-22 00:56:50 +0100

    implement profile page component andget prompts by user id route

---
+++ app/profile/loading.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Spinner from "@components/Spinner";
+
+const Loading = () => {
+  return (
+    <div className="w-full flex-center">
+      <Spinner />
+    </div>
+  );
+};
+
+export default Loading;
